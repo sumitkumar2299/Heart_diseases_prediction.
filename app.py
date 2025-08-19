d63f5ccfc08b9f3c50355f6be54d7ecc9b03fcfb
sumitkumar2299 <sumitofficialme@gmail.com>
2025-08-19 23:44:29 +0530

    solving error

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(BASE_DIR, "KNN_heart.pkl"), "rb") as f:
-    model = pickle.load(f)
+# with open(os.path.join(BASE_DIR, "KNN_heart.pkl"), "rb") as f:
+#     model = pickle.load(f)
 @st.cache_resource
 def load_model():
     try:
